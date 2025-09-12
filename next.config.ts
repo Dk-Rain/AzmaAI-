e2ee63510fe07efe68f2f9d397d744aade572ee6
SFARET TECHNOLOGIES <sfameitteam@gmail.com>
2025-09-12 12:15:02 +0000

    Error: Invalid src prop (https://generativelanguage.googleapis.com/v1bet

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'generativelanguage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
