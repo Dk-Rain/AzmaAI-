286175b62345f361fb6061ce45cbd1da7258bb05
SFARET TECHNOLOGIES <sfameitteam@gmail.com>
2025-09-15 07:31:54 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,25 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-            },
-            {
-                protocol: 'https',
-                hostname: 'media0.giphy.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'api.dicebear.com',
-            }
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "generativelanguage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media0.giphy.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
